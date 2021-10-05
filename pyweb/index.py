ab570a74efcdd90b71ad360c2c11b6ce68bc27f3
Jyoti Ranjan Sharma <appdeveloperubuntu@gmail.com>
2021-10-06 00:54:26 +0530

    sending you json
    reading a list of fruits from a txt file

--- pyweb/index.py
+++ pyweb/index.py
@@ -1,38 +1,22 @@
 import tornado.web
 import tornado.websocket
 import tornado.ioloop
+import json
 
 
-class basicRequestHandler(tornado.websocket.WebSocketHandler):  # Class to create a server to handle the request
+class listRequestHandler(tornado.websocket.WebSocketHandler):
     def get(self):
-        self.write('Hello, world, this is a Python on the backend')  # printing out the message to webpage
-
-class queryParamRequestHandler(tornado.websocket.WebSocketHandler):  # Class to create a server to handle the request
-    def get(self):
-        num = self.get_argument('num')
-        if (num.isdigit()):
-            r = "odd" if int(num)%2 != 0 else "even"
-            self.write(f"{num} is {r}")
-        else:
-            self.write(f"{num} is not a valid integer")
-
-
-class listRequestHandler(tornado.websocket.WebSocketHandler):  # Class to create a server to handle the request
-    def get(self):
-        self.render('index.html')
-
-
-class resourceParamRequestHandler(tornado.websocket.WebSocketHandler):  # Class to create a server to handle the request
-    def get(self, studentName, courseId):
-        self.write(f'Welcome {studentName} with course id {courseId}')
+        fh = open('D:/Studies/Hussein Nasser/Python on the Backend/pyweb/list.txt', 'r')
+        fruits = fh.read().splitlines()
+        for f in fruits:
+            print(f)
+        fh.close()
+        self.write(json.dumps(fruits))
 
 
 if __name__ == '__main__':
     app = tornado.web.Application([
-        (r'/', basicRequestHandler),  # Calling out the class with the / request
-        (r'/animal', listRequestHandler),
-        (r'/isEven', queryParamRequestHandler),
-        (r'/students/([a-z]+)/([0-9]+)', resourceParamRequestHandler)
+        (r'/fruits', listRequestHandler),
     ])
  
     port = 8888
